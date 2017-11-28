a006930c8509cfd24cfd1151bc902ae22e0746be
bklx <bartlomiej.kionka@luxmed.pl>
2017-11-28 12:36:15 +0100

    kalkulator

---
+++ Automatyzacja2017/ClassLibrary1/MathematicsTEST.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ClassLibrary1
+{
+    public class MathematicsTEST
+    {
+        [Fact]
+        public void Method_add_returns_sum_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Add(10, 20);
+
+            // assert
+            Assert.Equal(30, result);
+        }
+
+        [Fact]
+        public void Method_add_returns_add_of_given_values2()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Add(40, -20);
+
+            // assert
+            Assert.Equal(20, result);
+        }
+        [Fact]
+        public void Method_add_returns_sub_of_given_values()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Substract(40, -20);
+
+            // assert
+            Assert.Equal(60, result);
+        }
+        [Fact]
+        public void Method_add_returns_sub_of_given_values2()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Substract(100, 99);
+
+            // assert
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void Method_add_returns_divide_of_given_values1()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Divide(-10, -10);
+
+            // assert
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void Method_divide_returns__of_given_values2()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Divide(100, 0);
+
+            // assert
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void Method_multiply_returns__of_given_values1()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Multiply(100, -20);
+
+            // assert
+            Assert.Equal(-2000, result);
+        }
+        [Fact]
+        public void Method_multiply_returns__of_given_values2()
+        {
+            // arrange
+            var math = new Mathematics();
+
+            // act
+            var result = math.Multiply(6, -6);
+
+            // assert
+            Assert.Equal(-36, result);
+        }
+    }
+}
